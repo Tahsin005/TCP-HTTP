daf49c9914adc6d1af38274854d9edc74cc5a090
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-16 04:47:45 +0600

    json review

---
+++ HTTP-Client/2-json/2-decoding-json/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func getIssues(url string) ([]Issue, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+	defer res.Body.Close()
+
+	var issues []Issue
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&issues)
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, nil
+}
