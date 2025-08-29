61300c1d2acb3c2997d489244b48f5da9aa823be
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-29 15:42:23 +0600

    reading 8 bytes of data at a time and formatting the output line by line

--- TCP-HTTP/httpfromtcp/main.go
+++ TCP-HTTP/httpfromtcp/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,17 +18,43 @@ func main() {
 	defer file.Close()
 
 	buffer := make([]byte, 8)
+    var currentLine string
 
 	for {
 		bytesRead, err := file.Read(buffer)
 
 		if err == io.EOF {
 			if bytesRead > 0 {
-				fmt.Printf("%s\n", buffer[:bytesRead])
+				parts := strings.Split(string(buffer[:bytesRead]), "\n")
+				currentLine += parts[0]
+				if currentLine != "" {
+					fmt.Printf("read: %s\n", currentLine)
+				}
+				if len(parts) > 1 {
+					for _, part := range parts[1:] {
+						if part != "" {
+							fmt.Printf("read: %s\n", part)
+						}
+					}
+				}
 			}
+            fmt.Println("read: end")
 			break
 		}
 
-		fmt.Printf("read: %s\n", buffer[:bytesRead])
+		if bytesRead > 0 {
+			parts := strings.Split(string(buffer[:bytesRead]), "\n")
+			for i, part := range parts {
+				if i < len(parts) - 1 {
+					currentLine += part
+					if currentLine != "" {
+						fmt.Printf("read: %s\n", currentLine)
+						currentLine = ""
+					}
+				} else {
+					currentLine += part
+				}
+			}
+		}
 	}
 }
