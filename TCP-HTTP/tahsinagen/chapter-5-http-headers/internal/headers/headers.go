f53557396296f3d5785139c3c09fb7499361b96b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-09-23 23:36:02 +0600

    constraints

--- TCP-HTTP/tahsinagen/chapter-5-http-headers/internal/headers/headers.go
+++ TCP-HTTP/tahsinagen/chapter-5-http-headers/internal/headers/headers.go
@@ -3,15 +3,31 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if (ch >= 'A' && ch <= 'Z') ||
+			(ch >= 'a' && ch <= 'z') ||
+			(ch >= '0' && ch <= '9') {
+			found = true
+		}
+		switch ch {
+			case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+				found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+
+	return true
+}
 
 var rn = []byte("\r\n")
-
-func NewHeaders() Headers {
-	return map[string]string{}
-}
 
 func parseHeader(fieldLine []byte) (string, string, error) {
 	parts := bytes.SplitN(fieldLine, []byte(":"), 2)
@@ -23,13 +39,31 @@ func parseHeader(fieldLine []byte) (string, string, error) {
 	value := bytes.TrimSpace(parts[1])
 
 	if bytes.HasSuffix(name, []byte(" ")) {
-		return "", "", fmt.Errorf("malformed field name")
+		return "", "", fmt.Errorf("malformed header name")
 	}
 
 	return string(name), string(value), nil
 }
 
-func (h Headers) Parse(data []byte) (int, bool, error) {
+
+type Headers struct {
+	headers map[string]string
+}
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
+}
+
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
 	read := 0
 	done := false
 
@@ -51,8 +85,12 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 			return 0, false, err
 		}
 
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed field name")
+		}
+
 		read += idx + len(rn)
-		h[name] = value
+		h.Set(name, value)
 	}
 
 	return read, done, nil
