2a676d195fba3983557253f72fcd7c35842e5d45
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-09-26 00:44:26 +0600

    multiple values for a single header

--- TCP-HTTP/tahsinagen/chapter-5-http-headers/internal/headers/headers_test.go
+++ TCP-HTTP/tahsinagen/chapter-5-http-headers/internal/headers/headers_test.go
@@ -35,4 +35,12 @@ func TestHeaderParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069       \r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.NotNil(t, 0, n)
+	assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("Host"))
+	assert.False(t, done)
 }
