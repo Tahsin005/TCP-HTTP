f99b368b6bb837ece1bea5247c2b3b8f0bfe8278
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-09-29 08:00:34 +0600

    handling default http responses with headers

--- TCP-HTTP/tahsinagen/chapter-7-http-responses/internal/server/server.go
+++ TCP-HTTP/tahsinagen/chapter-7-http-responses/internal/server/server.go
@@ -4,16 +4,20 @@ import (
 	"fmt"
 	"io"
 	"net"
+
+	"tahsin.ferdous.exe/internal/response"
 )
 
 type Server struct {
 	closed bool
 }
 
-func runConnection(s *Server, conn io.ReadWriteCloser) {
-	out := []byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!")
-	conn.Write(out)
-	conn.Close()
+func runConnection(_s *Server, conn io.ReadWriteCloser) {
+	defer conn.Close()
+
+	headers := response.GetDefaultHeaders(0)
+	response.WriteStatusLine(conn, response.StatusOK)
+	response.WriteHeaders(conn, headers)
 }
 
 func runServer(s *Server, listener net.Listener) {
