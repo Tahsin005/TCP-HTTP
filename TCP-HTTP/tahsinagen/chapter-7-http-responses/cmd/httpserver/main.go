1433a190f063e124d97cce3f5782adb1be9f4797
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-10-02 11:04:03 +0600

    http responses - handler

--- TCP-HTTP/tahsinagen/chapter-7-http-responses/cmd/httpserver/main.go
+++ TCP-HTTP/tahsinagen/chapter-7-http-responses/cmd/httpserver/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"tahsin.ferdous.exe/internal/request"
+	"tahsin.ferdous.exe/internal/response"
 	"tahsin.ferdous.exe/internal/server"
 )
 
 const port = 42069
 
 func main() {
-	s, err := server.Serve(port)
+	s, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+		if req.RequestLine.RequestTarget == "/yourproblem" {
+			return &server.HandlerError{
+				StatusCode: response.StatusBadRequest,
+				Message: "Your problem is not my problem\n",
+			}
+		} else if req.RequestLine.RequestTarget == "/myproblem" {
+			return &server.HandlerError{
+				StatusCode: response.StatusInternalServerError,
+				Message: "Woopsie, my bad\n",
+			}
+		} else {
+			w.Write([]byte("All good, frfr\n"))
+		}
+		return nil
+	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
